b518f861b4c57a466e57b656d2a1234ab35b7cd9
Kathy Nguyen <kathyvng@gmail.com>
2018-06-15 05:01:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-  "apple is the best fruit".match(^[aeiou])
+  word.match(^[aeiou])
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  "underneath it all she was uplifting".match(un|ing)
+  text.match(un|ing)
 end
 
 def words_five_letters_long(text)
-  "Kathy loves AdamS".match(\w{5})
+  text.match(\w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  "Was it Peaceful? It was, out in the open field.".scan(^capitalized...punctuation?$)
+  text.scan(/^[A-Z].+[.?!"]$/)
 end
 
 def valid_phone_number?(phone)
-  ["888-555-1111", "(786) 888-1234", "8673451234"]numbers.scan(\d)
+  phone.match(\d)
 end
