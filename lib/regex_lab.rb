9d07d7a0541ce70c54cc0d00d1351ffa017201d9
Kathy Nguyen <kathyvng@gmail.com>
2018-06-15 04:50:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  "apple is the best fruit".match(^[aeiou])
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  "underneath it all she was uplifting".match(un|ing)
 end
 
 def words_five_letters_long(text)
-
+  "Kathy loves AdamS".match(\w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  "The beach is Peaceful!".scan(^capitalized...punctuation$)
 end
 
 def valid_phone_number?(phone)
-
+  "888-555-1111"match.scan(\d)
 end
